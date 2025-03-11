1fc8ff77ef8c402ae6a306bcd66423bb8bb63b80
zROCAc6e <114380949+zROCAc6e@users.noreply.github.com>
2025-03-11 02:18:18 -0700

    更新了 config.py 文件

---
+++ MAIN.js
@@ -0,0 +1,18 @@
+// 更新日志:
+//2023.03.19 v1.7.1 积分API接口修复
+//2022.12.12 v1.7.0 解决滑动验证问题
+
+// 学习强国 [学习小熊] PC端桌面浏览器扩展 当前目录内压缩包下载后解压使用
+// 安装扩展后，点击浏览器右上角图标自动打开 [https://www.xuexi.cn] 网站完成学习任务
+// 最新版本 [学习小熊] 火狐 Chrome 360 Edge QQ 浏览器扩展插件 扫码登录官网学习 Andriod/IOS设备均适用
+// 一个学习强国的帮手 用于学习强国官网自动学习 帮助一些成年人（或许未来包括孩子）快乐地完成来自学习强国的作业，让更多时间思考人生的意义
+// Telegram频道 TG: LearningThePooh
+alert("hellp,world");
+let urlMap = {
+    "index": "https://www.xuexi.cn",
+    "points": "https://pc.xuexi.cn/points/login.html",
+    "scoreApi": "https://pc-proxy-api.xuexi.cn/api/score/days/listScoreProgress?sence=score&deviceType=2",
+    "channelApi": "https://www.xuexi.cn/lgdata/",
+};
+// xuexi.cn自动学习 当前目录内压缩包下载后解压使用
+// 好好学习，天天向上 学习强国 浏览器扩展插件
