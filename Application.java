2d51436ed85132481e82bd673bc4b3541297401c
zROCAc6e <114380949+zROCAc6e@users.noreply.github.com>
2025-08-09 08:18:06 -0700

    添加了 用户认证 功能

---
+++ Application.java
@@ -0,0 +1,17 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    //获取完整代码联系QQ86050149 微信bysj5151
+    //获取完整代码联系QQ86050149 微信bysj5151
+    //获取完整代码联系QQ86050149 微信bysj5151
+    //获取完整代码联系QQ86050149 微信bysj5151
+    //获取完整代码联系QQ86050149 微信bysj5151
+
+}
